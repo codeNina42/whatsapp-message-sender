5dc81dda417f465b29c11235466c95b7b875a98e
md sajid ahsan seyam <129988169+codeNina42@users.noreply.github.com>
2025-10-06 13:33:56 +0600

    Add files via upload

---
+++ whatsapp/whatsapp.py
@@ -0,0 +1,9 @@
+import pywhatkit
+
+# Syntax: pywhatkit.sendwhatmsg(phone_no, message, hour, minute)
+pywhatkit.sendwhatmsg_instantly("+8801673895208", 
+                                "Hi, ami seyam abbu.." 
+                                "ami eta muloto programme kore coding er maddhome" 
+                                " tmk artificail inteligence diye message pathacchi ")
+
+
